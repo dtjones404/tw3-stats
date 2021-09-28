d4b0f716619b0a2288c77271e7e409406c0210c5
dtjones404 <dustintaylorjones@gmail.com>
2021-09-28 19:28:13 -0400

    Added summary controller and route.

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,3 +1,24 @@
-export default function handler(req, res) {
-  res.status(200).json(req.body);
+import summaryController from '../../lib/summaryController';
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const data = await summaryController.getSummaries();
+        res.status(200).json(data);
+      } catch (err) {
+        res.status(400).json({ success: false, data });
+      }
+      break;
+    case 'PUT':
+      try {
+        const updatedData = await summaryController.updateSummary(req.body);
+        res.status(200).json(updatedData);
+      } catch (err) {
+        res.status(400).json({ success: false });
+      }
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
 }

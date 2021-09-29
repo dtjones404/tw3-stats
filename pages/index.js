bc404183192fc901e58f60e4ecdc2b19e11d3e3a
dtjones404 <dustintaylorjones@gmail.com>
2021-09-29 18:40:49 -0400

    Updated styling, added header.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Faction, { factionNames } from '../models/Faction.js';
 import dbConnect from '../lib/dbConnect';
 import { FactionTable } from '../components/FactionTable.js';
+import styles from '../styles/Index.module.css';
 
 export default function Index({ tableData }) {
-  return <FactionTable factions={tableData} />;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Head>
+          <title>Total Warhammer Stats</title>
+          <meta
+            name="description"
+            content="Statistics for Total War: Warhammer III"
+          />
+        </Head>
+        <div>
+          <Image
+            className={styles.landingImage}
+            src="/public/header.jpg"
+            alt="The Dawn of Grand Cathay"
+            layout="fill"
+          />
+          <h1 className={styles.title}>Total Warhammer III Stats</h1>
+        </div>
+        <FactionTable factions={tableData} />
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
